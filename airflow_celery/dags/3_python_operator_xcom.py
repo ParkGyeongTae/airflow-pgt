fb5465d4d7ae7c705d7ef1864d27edb678d634ac
ParkGyeongTae <pgt0409@gmail.com>
2022-12-06 20:30:39 +0900

    update xcom file

--- airflow_celery/dags/3_python_operator_xcom.py
+++ airflow_celery/dags/3_python_operator_xcom.py
@@ -9,16 +9,11 @@ dag = DAG (
 	catchup = False,
 	tags = ['server_local', 'detail_test'],
 	description = 'Python Operator Sample',
-	default_args = {'owner': 'ParkGyeongTae', 'provide_context': True})
+	default_args = {'owner': 'ParkGyeongTae'})
 
 def func_xcom_push_1(**context):
     return context['task_instance'].xcom_push(key = 'xcom_push_key_1', value = 'xcom_push_value_1')
 
-# def func_xcom_push_2(**context):
-# 	xcom_value = 'xcom_push_value_2'
-#     return context['task_instance'].xcom_push(key = 'xcom_push_key_2', value = xcom_value)
-
 task_xcom_push_1 = PythonOperator(task_id = 'xcom_push_1', python_callable = func_xcom_push_1, dag = dag, do_xcom_push = True)
-# task_xcom_push_2 = PythonOperator(task_id = 'xcom_push_2', python_callable = func_xcom_push_2, dag = dag, do_xcom_push = True)
 
 task_xcom_push_1
